f46b00c2e06618bf3d74691bb310c0ecedff17db
MOHIT TIWARI <mohitt533@gmail.com>
2018-06-27 08:09:20 +0530

    SVR

---
+++ Regression/Support Vector Regression (SVR)/svr.py
@@ -0,0 +1,34 @@
+# Regression Template
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Position_Salaries.csv')
+X = dataset.iloc[:, 1:2].values
+y = dataset.iloc[:, 2:3].values
+
+#Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+sc_Y = StandardScaler()
+X = sc_X.fit_transform(X)
+y = sc_Y.fit_transform(y)
+
+# Fitting the SVR Model to the dataset
+from sklearn.svm import SVR
+regressor = SVR(kernel = 'rbf')
+regressor.fit(X, y)
+
+# Predicting a new result
+y_pred = sc_Y.inverse_transform(regressor.predict(sc_X.transform(np.array([[6.5]]))))
+
+# Visualising the SVR results 
+plt.scatter(X, y, color = 'red')
+plt.plot(X, regressor.predict(X), color = 'blue')
+plt.title('Truth or Bluff (SVR)')
+plt.xlabel('Position level')
+plt.ylabel('Salary')
+plt.show()
